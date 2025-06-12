151e1cd88850b23146cfe82f1dcfd2d31c6cc8ba
Hamed Ajorlou <hajorlou@ur.rochester.edu>
2025-06-12 11:18:58 -0400

    Fix: Add main.tsx and index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MyWebpage/', // Replace with your repository name
+  base: '/', // Remove repository name since we're using username.github.io
   plugins: [react()],
 })
