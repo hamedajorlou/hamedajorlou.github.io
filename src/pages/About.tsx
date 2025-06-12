8cc4ef2e679b35e218845e7722625d9d42eb7ec9
Hamed Ajorlou <hajorlou@ur.rochester.edu>
2025-06-12 17:49:24 -0400

    style: center content in Projects and About pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,6 +8,7 @@ const About = () => {
       sx={{ 
         display: 'flex', 
         alignItems: 'center',
+        justifyContent: 'center',
         maxWidth: '1800px'
       }}
     >
@@ -16,68 +17,67 @@ const About = () => {
         spacing={4} 
         sx={{ 
           alignItems: 'center',
-          pr: { xs: 2, md: 4 },
-          justifyContent: 'flex-end',
+          justifyContent: 'center',
+          maxWidth: '1200px'
         }}
       >
-        <Grid 
-          item 
-          xs={12} 
-          md={7} 
-          sx={{
-            pl: { md: 32 },
-          }}
-        >
+        <Grid item xs={12}>
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8 }}
           >
-            <Typography variant="h3" component="h1" gutterBottom>
+            <Typography variant="h3" component="h1" gutterBottom align="center">
               About Me
             </Typography>
           </motion.div>
+        </Grid>
 
+        <Grid item xs={12} md={6}>
           <motion.div
             initial={{ opacity: 0, x: -20 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8, delay: 0.2 }}
           >
             <Paper elevation={3} sx={{ p: 3, mb: 4 }}>
-              <Typography variant="h5" gutterBottom>
+              <Typography variant="h5" gutterBottom align="center">
                 Professional Background
               </Typography>
-              <Typography paragraph>
+              <Typography paragraph align="center">
                 [Your professional background and experience here]
               </Typography>
             </Paper>
           </motion.div>
+        </Grid>
 
+        <Grid item xs={12} md={6}>
           <motion.div
             initial={{ opacity: 0, x: 20 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8, delay: 0.4 }}
           >
             <Paper elevation={3} sx={{ p: 3, mb: 4 }}>
-              <Typography variant="h5" gutterBottom>
+              <Typography variant="h5" gutterBottom align="center">
                 Skills & Expertise
               </Typography>
-              <Typography paragraph>
+              <Typography paragraph align="center">
                 [List your technical skills and areas of expertise]
               </Typography>
             </Paper>
           </motion.div>
+        </Grid>
 
+        <Grid item xs={12} md={8}>
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.6 }}
           >
             <Paper elevation={3} sx={{ p: 3 }}>
-              <Typography variant="h5" gutterBottom>
+              <Typography variant="h5" gutterBottom align="center">
                 Education & Certifications
               </Typography>
-              <Typography paragraph>
+              <Typography paragraph align="center">
                 [Your educational background and certifications]
               </Typography>
             </Paper>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -32,6 +32,7 @@ const Projects = () => {
       sx={{ 
         display: 'flex', 
         alignItems: 'center',
+        justifyContent: 'center',
         maxWidth: '1800px'
       }}
     >
@@ -40,62 +41,53 @@ const Projects = () => {
         spacing={4} 
         sx={{ 
           alignItems: 'center',
-          pr: { xs: 2, md: 4 },
-          justifyContent: 'flex-end',
+          justifyContent: 'center',
+          maxWidth: '1200px'
         }}
       >
-        <Grid 
-          item 
-          xs={12} 
-          md={7} 
-          sx={{
-            pl: { md: 32 },
-          }}
-        >
+        <Grid item xs={12}>
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8 }}
           >
-            <Typography variant="h3" component="h1" gutterBottom>
+            <Typography variant="h3" component="h1" gutterBottom align="center">
               My Projects
             </Typography>
           </motion.div>
-
-          <Grid container spacing={4}>
-            {projects.map((project, index) => (
-              <Grid item key={project.id} xs={12} sm={6} md={4}>
-                <motion.div
-                  initial={{ opacity: 0, y: 20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.8, delay: index * 0.2 }}
-                >
-                  <Card>
-                    <CardMedia
-                      component="img"
-                      height="200"
-                      image={project.image}
-                      alt={project.title}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {project.title}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        {project.description}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small" color="primary" href={project.link} target="_blank">
-                        Learn More
-                      </Button>
-                    </CardActions>
-                  </Card>
-                </motion.div>
-              </Grid>
-            ))}
-          </Grid>
         </Grid>
+
+        {projects.map((project, index) => (
+          <Grid item key={project.id} xs={12} sm={6} md={4}>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: index * 0.2 }}
+            >
+              <Card>
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image={project.image}
+                  alt={project.title}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {project.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {project.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary" href={project.link} target="_blank">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </motion.div>
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );
